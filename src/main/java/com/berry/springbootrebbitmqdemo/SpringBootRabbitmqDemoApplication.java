ab1b9c470e6cfd969d787c5a68f52a61fddc41cd
Berry Cooper <caoxuean@seassoon.com>
2018-12-12 09:10:36 +0800

    refactor  main name

--- src/main/java/com/berry/springbootrebbitmqdemo/SpringBootRabbitmqDemoApplication.java
+++ src/main/java/com/berry/springbootrebbitmqdemo/SpringBootRabbitmqDemoApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootRebbitmqDemoApplication {
+public class SpringBootRabbitmqDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootRebbitmqDemoApplication.class, args);
+        SpringApplication.run(SpringBootRabbitmqDemoApplication.class, args);
     }
 }
